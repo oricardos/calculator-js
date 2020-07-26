9406232d94ad08cad5792d1429e2380a58c8e66a
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-07-26 00:36:55 -0300

    adding operations

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor() {
+
+        this._operation = []
         this._locale = 'pt-br'
         this._displayCalcEl = document.querySelector('#display')
         this._hourEL = document.querySelector('#hora')
@@ -7,7 +9,6 @@ class CalcController {
         this._currentDate
         this.initialize()
         this.initButtonsEvents()
-        this.addEventListenerAll()
     }
 
     initialize() {
@@ -25,6 +26,112 @@ class CalcController {
         })
     }
 
+    clearAll(){
+        this._operation = []
+    }
+
+    clearEntry(){
+        this._operation.pop()
+    }
+
+    getLastOperation(){
+        return this._operation[this._operation.length - 1]
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] =  value
+    }
+
+    isOperator(value) {
+        return (['+', '-', '*', '/', '%'].indexOf(value) > - 1)
+    }
+
+    addOperation(value){
+        console.log(this.getLastOperation())
+
+        if (isNaN(this.getLastOperation())) {
+            //string
+            if (this.isOperator(value)){
+                //change the operator
+                this.setLastOperation(value)
+            } else if (isNaN(value)){
+                //others
+                console.log(value)
+            } else {
+                this._operation.push(value)
+            }
+        } else {
+            //number
+            let newValue = this.getLastOperation().toString() + value.toString()
+            this.setLastOperation(parseInt(newValue))
+            
+        }
+
+        console.log(this._operation)
+    }
+
+    setError() {
+        this.displayCalc = 'ERROR'
+    }
+
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll()
+                break
+
+            case 'ce':
+                this.clearEntry()
+                break
+
+            case 'soma':
+                this.addOperation('+')
+                break
+
+            case 'subtracao':
+                this.addOperation('-')
+                break
+
+            case 'divisao':
+                this.addOperation('/')            
+                break
+
+            case 'multiplicacao':
+                this.addOperation('*')            
+                break
+
+            case 'porcento':
+                this.addOperation('%')            
+                break
+
+            case 'igual':
+            
+                break
+            
+            case 'ponto':
+                this.addOperation('.')
+                break
+
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value))
+                break
+
+            default:
+                this.setError()
+                break
+        }
+    }
+
     initButtonsEvents() {
         // pegando todos os botões
         let buttons = document.querySelectorAll('#buttons > g, #parts > g')
@@ -32,7 +139,9 @@ class CalcController {
         // varrendo botões e pegando seu valor
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace('btn-', ''))
+                let textBtn = (btn.className.baseVal.replace('btn-', ''))
+
+                this.execBtn(textBtn)
             })
 
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
