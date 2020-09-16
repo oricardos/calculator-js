aeafbc1e49b8b9f5971f6da2284b44a340bfd13c
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-09-15 22:37:31 -0300

    copy and paste

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -15,6 +15,30 @@ class CalcController {
         this.initKeyboard()
     }
 
+    pasteFromClipboard(){
+        document.addEventListener('paste', e => {
+            let text = e.clipboardData.getData('Text')
+
+            this.displayCalc = parseFloat(text)
+
+            console.log(text)
+        })
+    }
+
+    copyToClipboard(){
+        let input = document.createElement('input')
+
+        input.value = this.displayCalc
+
+        document.body.appendChild(input)
+
+        input.select()
+
+        document.execCommand("Copy")
+
+        input.remove()
+    }
+
     initialize() {
         // iniciando data e hora assim que entra na aplicação
         this.setDisplayDateTime()
@@ -24,6 +48,7 @@ class CalcController {
         }, 1000)
 
         this.setLastNumberToDisplay()
+        this.pasteFromClipboard()
 
     }
 
@@ -70,6 +95,10 @@ class CalcController {
                 case '8':
                 case '9':
                     this.addOperation(parseInt(e.key))
+                    break
+
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard()
                     break
             }
         })
