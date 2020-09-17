ab9ec93c3eb11fc93be76905c82bff3a736f30ab
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-09-16 23:29:57 -0300

    add audio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,9 @@
 class CalcController {
     constructor() {
 
+        this._audio = new Audio('click.mp3')
+        this._audioOnOff = false
+
         this._lastOperator = ''
         this._lastNumber = ''
 
@@ -50,11 +53,28 @@ class CalcController {
         this.setLastNumberToDisplay()
         this.pasteFromClipboard()
 
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio()
+            })
+        })
+
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff
+    }
+
+    playAudio(){
+        if (this._audioOnOff){
+            this._audio.currentTime = 0
+            this._audio.play()
+        }
     }
 
     initKeyboard(){
         document.addEventListener('keyup', e => {
-            console.log(e.key)
+            this.playAudio()
 
             switch (e.key) {
                 case 'Escape':
@@ -255,6 +275,8 @@ class CalcController {
     }
 
     execBtn(value) {
+        this.playAudio()
+
         switch (value) {
             case 'ac':
                 this.clearAll()
